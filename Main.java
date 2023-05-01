9b4dd0891944bbde5a23f3c8b2bbcea83b80a130
arielmygit <126427169+arielmygit@users.noreply.github.com>
2023-04-30 20:22:32 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Principal ventana = new Principal("Hola mundo AWT");
+    }
+}
